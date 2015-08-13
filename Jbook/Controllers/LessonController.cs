d290da41e60ed4f4e9b354bc12efebd71b9da954
kisflying <kisflying@gmail.com>
2015-08-13 15:58:28 +0800

    UPdatae

--- Jbook/Controllers/LessonController.cs
+++ Jbook/Controllers/LessonController.cs
@@ -6,12 +6,9 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Jbook.Controllers
-{
-    public class LessonController : Jbook.Base.BaseApiController
-    {
-        public IHttpActionResult Get(int id, String sid)
-        {
+namespace Jbook.Controllers {
+    public class LessonController : Jbook.Base.BaseApiController {
+        public IHttpActionResult Get(int id, String sid) {
             var lessonList = base.Ctx.Sql("select * from lesson where artisanId = @artisanId and [status] = @status")
                 .Parameter("artisanId", id)
                 .Parameter("status", sid)
@@ -20,8 +17,7 @@ namespace Jbook.Controllers
             return Ok(lessonList);
         }
 
-        public IHttpActionResult Get(int id)
-        {
+        public IHttpActionResult Get(int id) {
             var lesson = base.Ctx.Sql("select * from lesson where lessonId = @lessonId")
                 .Parameter("lessonId", id)
                 .QuerySingle<Lesson>();
@@ -29,13 +25,11 @@ namespace Jbook.Controllers
             return Ok(lesson);
         }
 
-        public IHttpActionResult PostApply(int lessonId, int customerId, bool isPaid)
-        {
+        public IHttpActionResult PostApply(int lessonId, int customerId, bool isPaid) {
             throw new NotImplementedException();
         }
 
-        public IHttpActionResult PutPaid(int lessonId, int customerId, bool isPaid)
-        {
+        public IHttpActionResult PutPaid(int lessonId, int customerId, bool isPaid) {
             throw new NotImplementedException();
         }
     }
