07312b0d9db1b04b97e987256864ae87e8b4f765
kisflying <kisflying@gmail.com>
2015-08-18 12:17:10 +0800

    Update

--- Jbook.UnitTest/ProductUnitTest.cs
+++ Jbook.UnitTest/ProductUnitTest.cs
@@ -8,13 +8,6 @@ namespace Jbook.UnitTest {
     public class ProductUnitTest {
         [TestMethod]
         public void TestMethod1() {
-            ProductController productController = new ProductController();
-
-            var result = productController.Get(1);
-
-            var responseText = result.ExecuteAsync(new CancellationToken(false)).Result.StatusCode.ToString();
-
-            Console.WriteLine(responseText);
         }
     }
 }
--- Jbook/Controllers/IndexController.cs
+++ Jbook/Controllers/IndexController.cs
@@ -1,13 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using Jbook.Base;
+using Jbook.Models;
 
 namespace Jbook.Controllers {
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
     public class IndexController : BaseApiController {
+        private readonly Product[] products = {
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 5},
+            new Product {Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+
         // GET: api/Index
         [HttpGet]
+        [Obsolete]
         public IEnumerable<string> Get() {
             return new[] {"value1", "value2", "valueN"};
+        }
+
+        [HttpGet]
+        [Obsolete]
+        public IHttpActionResult Get(int id) {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Obsolete]
+        public IHttpActionResult Get(int id, int sid, int tid) {
+            var z = "";
+
+            var books = Ctx.Sql("select * from stuff").QueryMany<Stuff>();
+
+            return Ok(books);
         }
     }
 }
