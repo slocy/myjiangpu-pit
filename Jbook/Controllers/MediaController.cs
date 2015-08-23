df2e06def0e723230cf6a14eddaa3408d0502b6d
kisflying <kisflying@gmail.com>
2015-08-23 18:22:32 +0800

    Update

---
+++ Jbook/Controllers/MediaController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.Http;
+using Jbook.Base;
+
+namespace Jbook.Controllers {
+    public class MediaController : BaseApiController {
+        [HttpGet]
+        public IHttpActionResult Get(int id, string sid, string tid, string fid) {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Jbook/Pipeline/ArtisanPipeline.cs
+++ Jbook/Pipeline/ArtisanPipeline.cs
@@ -13,7 +13,7 @@ namespace Jbook.Pipeline {
         public Artisan Get(int artisanId) {
             var artisan =
                 Ctx.Sql(
-                    "select top 1 ArtisanId, Name, Fullname, CellPhone, [Description], City, [Address] from dbo.Artisan where artisanId=@0",
+                    "select top 1 ArtisanId, Name, Fullname, StudioName, CellPhone, [Description], City, [Address] from dbo.Artisan where artisanId=@0",
                     artisanId).QuerySingle<Artisan>();
 
             return artisan;
